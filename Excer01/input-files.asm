1dad9278a978ba474447a29507229cad4e95a964
itzickw <80103682+itzickw@users.noreply.github.com>
2024-05-13 12:42:11 +0300

    display "enter path pls". space between inputs

---
+++ Excer01/input-files.asm
@@ -0,0 +1,32 @@
+InputA
+### BUY bread ###
+13.5
+### BUY cheese ###
+10.6
+$$$ SELL cheese $$$
+85.5
+### BUY milk ###
+24.8
+$$$ SELL chocolate $$$
+71.39999999999999
+### BUY milk ###
+22.400000000000002
+$$$ SELL chocolate $$$
+11.0
+
+InputB
+### BUY chocolate ###
+12.5
+$$$ SELL eggs $$$
+621.0
+### BUY cheese ###
+10.6
+$$$ SELL sugar $$$
+82.5
+$$$ SELL flour $$$
+25.2
+### BUY milk ###
+11.100000000000001
+
+TOTAL BUY: 105.5
+TOTAL SELL: 896.6
